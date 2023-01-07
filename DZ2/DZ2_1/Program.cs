99ae77bbcf963420ace0065ff2f830abf9df5402
Viktorya <fedya47@yandex.ru>
2023-01-07 21:04:17 +0400

    ready dz2_1

--- DZ2/DZ2_1/Program.cs
+++ DZ2/DZ2_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число, и я напишу вторую цифру этого числа");
+int a = Convert.ToInt32 (Console.ReadLine());
+
+if ( a < 100 || a > 999 ) 
+{
+    Console.WriteLine("Я что, шутка для тебя? Я просила трехзначное число!");
+}
+
+else 
+{
+    int a2 = (a % 100)/10;
+    Console.WriteLine(a2);
+}
+
+
