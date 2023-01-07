fede48cb1604b535c87c631c0d96a8abb70e6483
Viktorya <fedya47@yandex.ru>
2023-01-07 19:54:42 +0400

    ready dz2

--- DZ1/DZ1_2/Program.cs
+++ DZ1/DZ1_2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32 (Console.ReadLine());
+
+if ( a > b && a > c ) 
+{
+    Console.WriteLine($"Самое большое число = {a}");
+}
+else if ( b > a && b > c )
+{
+    Console.WriteLine($"Самое большое число = {b}");
+}
+else Console.WriteLine($"Самое большое число = {c}");
