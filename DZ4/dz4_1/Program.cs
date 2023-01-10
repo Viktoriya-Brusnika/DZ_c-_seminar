f3f538ef71bec15c6a06ead9bbad04c7ece335ce
Viktorya <fedya47@yandex.ru>
2023-01-11 01:09:39 +0400

    add dz4_1

---
+++ DZ4/dz4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"А в степен В равна {Expon(A, B)}");
+
+int Expon (int x, int y)
+{
+    int res = 1;
+    for (int i = 1; i < y+1; i++)
+    {
+        res = res * x;
+    }
+    return res;
+}
