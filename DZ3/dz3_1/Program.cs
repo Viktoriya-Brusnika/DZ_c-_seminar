d652d35b17baeb48fe9aad4f54673d810060ed91
Viktorya <fedya47@yandex.ru>
2023-01-14 15:54:01 +0400

    add dz4_1

--- DZ3/dz3_1/Program.cs
+++ DZ3/dz3_1/Program.cs
@@ -4,10 +4,39 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число");
-char[] a = Console.ReadLine().ToCharArray();
+// Console.WriteLine("Введите пятизначное число");
+// char[] a = Console.ReadLine().ToCharArray();
 
 
-if (a.Length > 5) Console.WriteLine("ввели неверные данные");
-else if (a[0] == a[4] || a[1] == a[3]) Console.WriteLine("Да, это палиндром");
-else Console.WriteLine("Нет, это не палиндром");
+// if (a.Length > 5) Console.WriteLine("ввели неверные данные");
+// else if (a[0] == a[4] || a[1] == a[3]) Console.WriteLine("Да, это палиндром");
+// else Console.WriteLine("Нет, это не палиндром");
+
+
+ using System;
+
+namespace PalindromeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a five-digit number:");
+            int number = int.Parse(Console.ReadLine());
+            int firstDigit = number / 10000;
+            int secondDigit = (number / 1000) % 10;
+            int thirdDigit = (number / 100) % 10;
+            int fourthDigit = (number / 10) % 10;
+            int fifthDigit = number % 10;
+
+            if (firstDigit == fifthDigit && secondDigit == fourthDigit)
+            {
+                Console.WriteLine("The number is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not a palindrome.");
+            }
+        }
+    }
+}
---
+++ DZ4/dz4_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 целых 
+// чисел с клавиатуры и далее выводит массив на экран в одну строку.
+
+
+int[] array = new int[8];
+InputNumb(8, array);
+PrintArray(array);
+
+int[] InputNumb(int x, int[] array)
+{
+    for (int i = 0; i < x; i++) 
+    {
+        Console.WriteLine($"Введите {i} элемент массива");
+        array[i] = array[i] + Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    for (int i=0;i<array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
