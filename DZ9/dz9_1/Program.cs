e972d0c675b01107f37f71f211545c32a2850fcc
Viktorya <fedya47@yandex.ru>
2023-01-19 23:52:32 +0400

    add dz9_1

---
+++ DZ9/dz9_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите натуральное число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AllNumber(N));
+
+
+string AllNumber(int N)
+{
+    if (N==1) return $"1";
+    return  $"{N} " + AllNumber(N-1);
+}
